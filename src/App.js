a5d7f38473e9efd3182d5b0faa64dd7efc86a77c
NABILAFZAL19 <nabilafzal19@gmail.com>
2021-07-06 16:18:21 +0530

    New Changes

--- src/App.js
+++ src/App.js
@@ -14,11 +14,30 @@ const notesReducer = (prevState,action)=>{
        case 'ADD_NOTE':{
          const newState = {
           lastNoteCreated: new Date().toTimeString().slice(0,8),
-          totalNotes: prevState.notes.length,
+          totalNotes: prevState.notes.length +1,
           notes:  [...prevState.notes,action.payload],
          };
          console.log(newState);
          return newState;
+       }
+
+       case 'DELETE_NOTE':{
+        // for(note of notes)
+        // {
+        //   if(note.id === action.payload.id)
+
+        // }
+      const  updatedNote ={
+          ...prevState,
+          totalNotes:prevState.notes.length-1,
+          notes: prevState.notes.filter(note=>note.id!==action.payload)
+
+        }
+        return updatedNote;
+        
+
+  
+         
        }
        default:
             return;
@@ -28,6 +47,7 @@ const notesReducer = (prevState,action)=>{
  const App = () => {
    const [noteInput,setNoteInput] = useState('');
    const [notesState,dispatch] = useReducer(notesReducer,intialNotesState)
+   
    const addNote = (event)=>{
     event.preventDefault();
     console.log(notesState)
@@ -39,6 +59,7 @@ const notesReducer = (prevState,action)=>{
        text: noteInput,
        rotate:Math.floor(Math.random() *20),
      };
+   
      dispatch({type:'ADD_NOTE' ,payload:newNote});
      setNoteInput("");
 
@@ -54,18 +75,33 @@ const notesReducer = (prevState,action)=>{
       event.stopPropagation();
       event.preventDefault();
    }
+   const doubleClickHandler = (id) =>{
+    //  const deleteNote ={
+    //    id:notesState.notes.map(note=>(note.id)),
+    
+       
+    
+    // }
+    dispatch({type:'DELETE_NOTE', payload:id})
+    // const modified = 
+  
+    // console.log(deleteNote.id)
+    console.log('hey')
+  
+   }
   return (
     <div className="app" onDragOver={dragOver}>
-      <h1>Sticky Notes</h1>
+      <h1>Sticky Notes {notesState.totalNotes}</h1>
 
       <form onSubmit={addNote} className="note-form"> 
        <textarea placeholder="create your note here" value={noteInput}onChange={(event)=>setNoteInput(event.target.value)}></textarea>
         <button>Add</button>
       </form>
      {notesState.notes.map(note =>(
-        <div className="note" style={{transform:`rotate(${note.rotate}deg)`}} draggable="true" onDragEnd={dropNote}>
+        <div className="note" style={{transform:`rotate(${note.rotate}deg)`}} draggable="true" onDragEnd={dropNote} onDoubleClick={()=>doubleClickHandler(note.id)}>
           <pre className="text">{note.text}</pre>
           </div>
+       
      ))}
     </div>
   )
